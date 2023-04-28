de91ae89fa3cb265a47d762ad3792b5cff09bce6
James Maclennan <myemail@example.com>
2023-04-27 22:02:27 -0600

    chore: utils

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+const name = "business name"
+const phone = 1234567890;
+const email = 'email@email.email'
+const address = ['123 Main Street', 'Beaumont, Alberta', 'T4X 1Z4, Canada']
+const socials = {
+  facebook: 'https://www.facebook.com/',
+  instagram: 'https://www.instagram.com/',
+}
+
+
+export const config = {
+  name,
+  phone,
+  email,
+  address,
+  socials
+}
---
+++ src/shared/utils.ts
@@ -0,0 +1,26 @@
+export const formatPhoneNumber = (phoneNumber: number) => {
+  const cleaned = ('' + phoneNumber).replace(/\D/g, '')
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+  }
+  return null
+}
+
+export const formatDollarAmount = (amount: number) => {
+  return '$' + amount.toFixed(2)
+}
+
+// convert a 24 hour time to 12 hour time
+export function convertTime(time: number | string): string {
+  const t = Number(time);
+  if (time === 0) {
+    return "12:00AM";
+  } else if (t < 12) {
+    return `${time}:00AM`;
+  } else if (t === 12) {
+    return "12:00PM";
+  } else {
+    return `${t - 12}:00PM`;
+  }
+}
