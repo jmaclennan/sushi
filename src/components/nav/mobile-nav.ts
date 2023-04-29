7a2d6043848a03e478d9b4e2510e53a43c27e18f
James Maclennan <jmaclennan@fastmail.com>
2023-04-29 14:38:01 -0700

    chore: replace right with translateX on mobile nav

--- src/components/nav/mobile-nav.ts
+++ src/components/nav/mobile-nav.ts
@@ -11,21 +11,22 @@ const app = {
   close: function () {
     gsap.to(this.elements.drawer, {
       duration: 0.25,
-      right: -this.state.drawerWidth,
+      x: this.state.drawerWidth,
       ease: 'power2.inOut',
     });
     this.elements.trigger.classList.remove('tham-active')
     this.state.isOpen = false;
   
   },
-  toggle: function(state?: boolean) {
+  toggle: function () {
+    console.log('toggle', this.state.isOpen)
     gsap.to(this.elements.drawer, {
       duration: 0.25,
-      right: this.state.isOpen ? -this.state.drawerWidth : 0,
+      x: this.state.isOpen ? this.state.drawerWidth : 0,
       ease: 'power2.inOut',
     });
     this.elements.trigger.classList.toggle('tham-active')
-    this.state.isOpen = state || !this.state.isOpen;
+    this.state.isOpen = !this.state.isOpen;
   },
   init: function () {
     if (!this.elements.trigger && !this.elements.drawer) {
@@ -35,14 +36,14 @@ const app = {
 
     // set initial state
     this.state.drawerWidth = app.elements.drawer.offsetWidth;
-    this.elements.drawer.style.right = `-${this.state.drawerWidth}px`
+    // this.elements.drawer.style.translate = `translateX(${this.state.drawerWidth}px)`
     
     this.elements.trigger.addEventListener('click', () => app.toggle());
     
     // close if user clicks outside drawer
     document.addEventListener('click', (e) => {
       if (this.state.isOpen && !this.elements.drawer.contains(e.target as Node) && !this.elements.trigger.contains(e.target as Node)) {
-        this.toggle(false);
+        this.toggle();
       }
     });
     
@@ -52,12 +53,12 @@ const app = {
         this.close()
       } else {
         this.state.drawerWidth = app.elements.drawer.offsetWidth;
-        this.elements.drawer.style.right = `-${this.state.drawerWidth}px`;
+        this.elements.drawer.style.transform = `translateX(${this.state.drawerWidth}px)`;
       }
     });
     
     this.elements.drawer.classList.remove('pointer-events-none', 'opacity-0');
-    this.elements.drawer.style.right = `-${this.state.drawerWidth}px`
+    this.elements.drawer.style.transform = `translateX(${this.state.drawerWidth}px)`
   }
 }
 
