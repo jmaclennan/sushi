722e90916f099c12555919df42f431a7b63bd9f6
James Maclennan <jmaclennan@fastmail.com>
2023-04-30 08:34:45 -0700

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://localhost:3000',
   integrations: [tailwind({
     config: {
       path: 'src/theme/tailwind.config.cjs'
     }
-  }), image()]
+  }), image(), sitemap()]
 });
