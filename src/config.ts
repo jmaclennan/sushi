af4ef3f56874366048e77532e140e85b1dc35b5a
James Maclennan <jmaclennan@fastmail.com>
2023-04-30 19:43:57 -0700

    chore: undo upstream test

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ const phone = 1234567890;
 const email = 'email@email.email'
 const address = ['123 Main Street', 'Beaumont, Alberta', 'T4X 1Z4, Canada']
 const socials = {
-  facebook: 'https://www.facebook.com/test',
-  instagram: 'https://www.instagram.com/test',
+  facebook: 'https://www.facebook.com/',
+  instagram: 'https://www.instagram.com/',
 }
 
 type NavItem = {
