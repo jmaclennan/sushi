ece3be2423e9da254b45073f2b8f087f06f63c10
James Maclennan <jmaclennan@fastmail.com>
2023-05-24 15:09:23 -0700

    style: adjust default font sizes

--- src/theme/fonts.ts
+++ src/theme/fonts.ts
@@ -6,14 +6,15 @@ const fontFamily = {
 
 const fontSize = {
   xs: ['.75rem', { lineHeight: '1rem' }],
-  sm: ['.875rem', { lineHeight: '1.25rem' }],
-  base: ['1.125rem', { lineHeight: '1.5rem' }],
-  lg: ['1.5rem', { lineHeight: '1.875rem' }],
-  xl: ['2.5rem', { lineHeight: '3.125rem' }],
+  sm: ['.9rem', { lineHeight: '1.6rem' }],
+  base: ['1.35rem', { lineHeight: '1.85rem' }],
+  lg: ['2rem', { lineHeight: '2.5rem' }],
+  xl: ['2.75rem', { lineHeight: '3.125rem' }],
   '2xl': ['3.75rem', { lineHeight: '4.375rem' }],
+  '3xl': ['4.5rem', { lineHeight: '5.625rem' }],
+  '4xl': ['6rem', { lineHeight: '7.5rem' }],
   '2xs': ['0.625rem', { lineHeight: '0.75rem' }],
 }
-
 const fonts = {
 	fontFamily,
 	fontSize,
