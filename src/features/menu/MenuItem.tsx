c1bb7c03f152d3d4f640b9268d2fb1d4b467d2be
James Maclennan <jmaclennan@fastmail.com>
2023-06-22 19:49:24 -0700

    style: style hover states and add to menu functionality

--- src/features/menu/MenuItem.tsx
+++ src/features/menu/MenuItem.tsx
@@ -1,5 +1,7 @@
 import type { CartItem } from "@/components/Cart/Cart";
 import { useLocalStorage } from "usehooks-ts";
+import { CgAddR } from "react-icons/cg";
+import { FaPepperHot } from "react-icons/fa";
 
 type MenuItemProps = {
   id: number;
@@ -38,24 +40,29 @@ export const MenuItem: React.FC<MenuItemProps> = ({
 
   return (
     <li
-      className="flex space-between w-full border-solid border-b border-neutral-300 border-b-1 space-x-4 py-4 md:py-2 cursor-pointer hover:bg-neutral-100"
+      className="w-full py-4 md:py-2 cursor-pointer hover:bg-neutral-100/50 group px-2 pr-8"
       onClick={handleClick}
     >
-      <div className="text-neutral-700 uppercase text-sm font-bold flex items-start pt-1">
-        <span>{category}</span>
-        <span>{id}</span>
-      </div>
-      <div className="flex flex-col">
-        <div className="flex ">
-          <span className="text-stone-600">
-            {name} {spicy ? "x" : ""}
-          </span>
+      <div className="flex space-between w-full space-x-4">
+        <div className="text-neutral-700 uppercase text-sm font-bold flex pt-1 items-start space-x-2 justify-start">
+          <CgAddR className="text-[20px] opacity-0 group-hover:opacity-100 text-success pt-1" />
+          <div className="flex">
+            <span>{category}</span>
+            <span>{id}</span>
+          </div>
         </div>
-        {description && <span className="text-sm">{description}</span>}
-      </div>
-      <div className="text-neutral-600 whitespace-nowrap text-right flex-grow flex justify-end">
-        <span>$</span>
-        <span>{price}</span>
+        <div className="flex flex-col">
+          <div className="flex ">
+            <span className="text-stone-600">
+              {name} {spicy && <FaPepperHot className="text-error" />}
+            </span>
+          </div>
+          {description && <span className="text-sm">{description}</span>}
+        </div>
+        <div className="text-neutral-600 whitespace-nowrap text-right flex-grow flex justify-end">
+          <span>$</span>
+          <span>{price}</span>
+        </div>
       </div>
     </li>
   );
